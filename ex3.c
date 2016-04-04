dc1e590d352797bf8c5e0a0c65f7911f6cdd2138
Jorge Edson Ribeiro da Silva Neto <jorge.ed.ribeiro00@gmail.com>
2016-04-04 20:24:34 -0300

    Esqueleto basico

---
+++ ex3.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int a[5];
+int top = -1;
+int choice, value;
+char Y;
+
+void valor()
+{
+    if(top==4)
+        printf("Pilha cheia");
+    else
+    {
+        top=top+1;
+        a[top]=value;
+    }
+}
+
+void pop()
+{
+    if(top==-1)
+        printf("Insira algum valor.");
+    else
+        top=top-1;
+}
+
+void display()
+{
+    int i;
+    if(top==-1)
+        printf("\nNada ainda.");
+    else
+    {
+        printf("\nAte agora:\n");
+        for(i=0; i<=top; i++)
+        {
+            printf("%d\n", a[i]);
+        }
+    }
+}
+
+int operacao()
+{
+    int X;
+    char M, D, P, S;
+
+    switch(Y)
+    {
+        case M:
+            X = a[top]*a[top-1];
+            printf("%d", X);
+            break;
+        case D:
+            X = a[top]/a[top-1];
+            print("%d", X);
+            break;
+        case P:
+            X = a[top]+a[top-1];
+            print("%d", X);
+            break;
+        case S:
+            X = a[top]-a[top-1];
+            print("%d", X);
+            break;
+    }
+}
+
+int main()
+{
+
+    do
+    {
+        printf("\n1.Digitar Valor;");
+        printf("\n2.Escolha Operacao;");
+        printf("\n3.Desfazer;");
+        printf("\n4.Display;");
+        printf("\n0.Exit;");
+        printf("\nO que deseja fazer? ");
+        scanf("%d", &choice);
+
+        if(choice==1)
+        {
+            printf("\nEnter the value: ");
+            scanf("%d", &value);
+            valor(value);
+        }
+
+        if(choice==2)
+        {
+            printf("\nM Multiplicacao;");
+            printf("\nD Divizao;");
+            printf("\nP Soma;");
+            printf("\nS Subtracao;");
+            printf("\nQual sua escolha? ");
+            Y = getchar();
+            operacao();
+        }
+
+        if(choice==3)
+            pop();
+
+        if(choice==4)
+            display();
+    }
+    while(choice!=0);
+    getch();
+
+    return 0;
+}
+
